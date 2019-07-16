7275776d77be0cda035fdaf299b13782db6b6cac
Luis Angel Aguilar Peña <luisaguilar@Luiss-MacBook-Pro.local>
2019-07-16 16:19:12 -0500

    Added a Card component with several examples

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "rbx/index.css";
 import { Container } from "rbx";
 import NotificationExample from "./NotificationExample";
 import BreadcrumbExample from "./BreadcrumbExample";
+import CardExample from "./CardExample";
 
 const App = () => {
   return (
     <Container>
       <NotificationExample />
       <BreadcrumbExample />
+      <CardExample />
     </Container>
   );
 }
